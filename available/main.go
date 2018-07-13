a200d99b149aaae98715f6bf40e9ddc533229c2e
EG-easy <1018eguchi@gmail.com>
2018-07-13 10:57:35 +0900

    add new file

---
+++ available/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net"
+	"bufio"
+	"strings"
+	"os"
+	"fmt"
+	"log"
+	"time"
+)
+
+func exsits(domain string) (bool, error){
+	const whoisServer string = "com.whois-servers.net"
+	conn, err := net.Dial("tcp", whoisServer+":43")
+	if err !=nil{
+		return false, err
+	}
+	defer conn.Close()
+	conn.Write([]byte(domain + "\r\n"))
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan(){
+		if strings.Contains(strings.ToLower(scanner.Text()), "no match"){
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+var marks = map[bool]string{true:" OK", false:" NG"}
+func main(){
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan(){
+		domain := s.Text()
+		fmt.Print(domain, "")
+		exist, err := exsits(domain)
+		if err !=nil{
+			log.Fatalln(err)
+		}
+		fmt.Println(marks[!exist])
+		time.Sleep(1 *time.Second)
+	}
+}
+
